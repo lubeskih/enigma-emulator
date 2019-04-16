893c19c96c81bd3773355be34290b2bdf7891268
lubeskih <lubeskih@gmail.com>
2019-04-16 22:31:32 +0200

    replaces layout with app

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Layout from './Layout';
+import App from './app';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Layout />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
