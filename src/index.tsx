c0f2fe21fb99024744bbcc5550dbf109ac32f42f
Hristijan Lubeski <lubeskih@gmail.com>
2019-08-19 03:11:38 +0200

    add mobile device detection

--- src/index.tsx
+++ src/index.tsx
@@ -5,4 +5,25 @@ import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<Enigma />, document.getElementById("root"));
 
+function detectmob() {
+  if (
+    navigator.userAgent.match(/Android/i) ||
+    navigator.userAgent.match(/webOS/i) ||
+    navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPad/i) ||
+    navigator.userAgent.match(/iPod/i) ||
+    navigator.userAgent.match(/BlackBerry/i) ||
+    navigator.userAgent.match(/Windows Phone/i)
+  ) {
+    if (
+      !window.confirm(
+        "My poor script has detected that you may be using a mobile device (sorry if not) to view this page. Keep in mind that the simulator does not work on mobile devices.\n\n'CANCEL' - will redirect you to https://google.com\n\n'OK' - will dismiss this message"
+      )
+    ) {
+      window.location.href = "https://www.google.com/";
+    }
+  }
+}
+
 serviceWorker.unregister();
+detectmob();
